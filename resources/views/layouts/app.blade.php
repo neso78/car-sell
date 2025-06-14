9ca395520d89bf0b8e4d4cd74bea16310fb8517e
neso <neso7878@example.com>
2025-06-14 18:01:43 +0200

    End first part

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,5 @@
 <x-base-layout title={{$title}}>
     <x-layouts.header/>
     {{$slot}}
-    <footer>
-        <a href="#" class="href">APP BLADE LINK | " </a>
-        {{-- Prikaz dodatnog sadržaja ako je prosleđen $footerLinks --}}
-        {{ $footerLinks }}
-    </footer>
 </x-base-layout>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
@@ -8,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('', [HomeController::class,'index'])->name('home');
 Route::get('signup', [RegisterController::class,'signup'])->name('signup');
 Route::get('login', [LoginController::class,'login'])->name('login');
+
+Route::get('car/search',[CarController::class,'search'])->name('car.search');
+Route::resource('car',CarController::class);
